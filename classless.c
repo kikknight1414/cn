5e7b95e2408b322b48bbcc74819be359ac22f545
kikknight1414 <141972923+kikknight1414@users.noreply.github.com>
2024-01-18 09:29:25 +0530

    Update classless.c

--- classless.c
+++ classless.c
@@ -3,36 +3,36 @@
 #include <string.h>
 
 char *ip_classless(char *ip_address) {
-    int i, j, k;
-    int octets[4];
-    char *mask = (char *)malloc(sizeof(char) * 16);
-    int bits = 0;
+int i, j, k;
+int octets[4];
+char *mask = (char *)malloc(sizeof(char) * 16);
+int bits = 0;
 
-    for (i = 0, j = 0, k = 0; i < strlen(ip_address); i++) {
-        if (ip_address[i] == '.') {
-            octets[j++] = atoi(ip_address + k);
-            k = i + 1;
-        }
-    }
-    octets[j] = atoi(ip_address + k);
+for (i = 0, j = 0, k = 0; i < strlen(ip_address); i++) {
+if (ip_address[i] == '.') {
+octets[j++] = atoi(ip_address + k);
+k = i + 1;
+}
+}
+octets[j] = atoi(ip_address + k);
 
-    for (i = 0; i < 4; i++) {
-        int octet = octets[i];
-        for (j = 7; j >= 0; j--) {
-            if (octet & (1 << j)) {
-                bits++;
-            } else {
-                break;
-            }
-        }
-    }
+for (i = 0; i < 4; i++) {
+int octet = octets[i];
+for (j = 7; j >= 0; j--) {
+if (octet & (1 << j)) {
+bits++;
+} else {
+break;
+}
+}
+}
 
-    sprintf(mask, "%s/%d", ip_address, bits);
-    return mask;
+sprintf(mask, "%s/%d", ip_address, bits);
+return mask;
 }
 
 int main() {
-    char ip_address[16] = "192.168.0.0";
-    printf("Classless address: %s\n", ip_classless(ip_address));
-    return 0;
+char ip_address[16] = "192.168.0.0";
+printf("Classless address: %s\n", ip_classless(ip_address));
+return 0;
 }
