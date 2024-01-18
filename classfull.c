458e8d716e5719cdbc645f2754f9f079b89e8a39
kikknight1414 <141972923+kikknight1414@users.noreply.github.com>
2024-01-18 09:28:46 +0530

    Update classfull.c

--- classfull.c
+++ classfull.c
@@ -3,45 +3,35 @@
 #include <string.h>
 
 void main() {
-    char ip[16], temp[16], flag = 0;
-    printf("Enter the IP Address : ");
-    scanf("%s", ip);
-    
-    int n = strlen(ip);
-    int x = 0, y = 0;
-
-    while (ip[x] != '.')
-        temp[y++] = ip[x++];	
-
-    if (atoi(temp) > 0 && atoi(temp) < 128) {
-        printf("\nIP Address belongs to Class A \n");
-    } else if (atoi(temp) > 127 && atoi(temp) < 192) {
-        printf("\nIP Address belongs to Class B \n");
-        y = 0;
-        temp[y++] = ip[x++];
-        while (ip[x] != '.') 
-            temp[y++] = ip[x++];
-    } else if (atoi(temp) > 191 && atoi(temp) < 224) {
-        printf("\nIP Address belongs to Class C \n");
-        y = 0;
-        temp[y++] = ip[x++];
-        while (ip[x] != '.') 
-            temp[y++] = ip[x++];
-        temp[y++] = ip[x++];
-        while (ip[x] != '.') 
-            temp[y++] = ip[x++];
-    } else {
-        printf("\nIP Address belongs to Class D/E \n");
-        flag = 1;
-    }
-    
-    temp[x++] = '\0';
-    
-    if (flag == 0) {
-        printf("\nNetwork ID : %s\n", temp);
-        printf("\nHost ID : ");
-        while (ip[x] != '\0') 
-            printf("%c", ip[x++]);
-        printf("\n\n");
-    }
+char ip[16], temp[16], flag = 0;
+printf("Enter the IP Address : ");
+scanf("%s", ip);
+int n = strlen(ip);
+int x = 0, y = 0;
+while (ip[x] != '.') temp[y++] = ip[x++];
+if (atoi(temp) > 0 && atoi(temp) < 128) {
+printf("\nIP Address belongs to Class A \n");
+} else if (atoi(temp) > 127 && atoi(temp) < 192) {
+printf("\nIP Address belongs to Class B \n");
+y = 0;
+temp[y++] = ip[x++];
+while (ip[x] != '.') temp[y++] = ip[x++];
+} else if (atoi(temp) > 191 && atoi(temp) < 224) {
+printf("\nIP Address belongs to Class C \n");
+y = 0;
+temp[y++] = ip[x++];
+while (ip[x] != '.') temp[y++] = ip[x++];
+temp[y++] = ip[x++];
+while (ip[x] != '.') temp[y++] = ip[x++];
+} else {
+printf("\nIP Address belongs to Class D/E \n");
+flag = 1;
+}
+temp[x++] = '\0';
+if (flag == 0) {
+printf("\nNetwork ID : %s\n", temp);
+printf("\nHost ID : ");
+while (ip[x] != '\0') printf("%c", ip[x++]);
+printf("\n\n");
+}
 }
